9bf0442864a970cd93fb9898804cfc400fe653d7
Ranjith <144647022+ranjithuniversity@users.noreply.github.com>
2023-09-15 18:03:28 +0530

    Add files via upload

---
+++ Unit 2 challenge 2.py
@@ -0,0 +1,16 @@
+class Player:…
+
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Create objects of Batsman and Bowler classes and call the play() method for each
+batsman = Batsman()
+bowler = Bowler()
+
+batsman.play()  # Output: "The batsman is batting."
+bowler.play()   # Output: "The bowler is bowling."
